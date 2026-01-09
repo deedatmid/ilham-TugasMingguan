e4f5f93f34070dfd570b3f1988b293dc940f6cf5
deedatmid <deedatalayyubi@gmail.com>
2026-01-10 02:39:53 +0700

    middleware auth login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('title', 'Login')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">Login</div>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login.submit') }}">
+                        @csrf
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\LoginController;
 use App\Models\Berita;
 use Illuminate\Support\Facades\Route;
 
@@ -25,17 +26,22 @@ Route::get('/profile', function () {
 Route::get('/berita',[BeritaController::class, 'index']);
 Route::get('/berita/{slug}', [BeritaController::class, 'tampildata']);
 
-Route::get('/mahasiswa',[MahasiswaController::class, 'index'])->name('mahasiswa');
+Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/tambahmahasiswa',[MahasiswaController::class, 'tambahmahasiswa'])->name('tambahmahasiswa');
-Route::POST('/insertdata',[MahasiswaController::class, 'insertdata'])->name('insertdata');
+Route::middleware('auth')->group(function () {
+    Route::get('/mahasiswa',[MahasiswaController::class, 'index'])->name('mahasiswa');
 
+    Route::get('/tambahmahasiswa',[MahasiswaController::class, 'tambahmahasiswa'])->name('tambahmahasiswa');
+    Route::POST('/insertdata',[MahasiswaController::class, 'insertdata'])->name('insertdata');
 
-Route::get('/tampildata/{id}',[MahasiswaController::class, 'tampildata'])->name('tampildata');
+    Route::get('/tampildata/{id}',[MahasiswaController::class, 'tampildata'])->name('tampildata');
 
-Route::POST('/editdata/{id}',[MahasiswaController::class, 'editdata'])->name('editdata');
+    Route::POST('/editdata/{id}',[MahasiswaController::class, 'editdata'])->name('editdata');
 
-Route::get('/deletedata/{id}',[MahasiswaController::class, 'deletedata'])->name('deletedata');
+    Route::get('/deletedata/{id}',[MahasiswaController::class, 'deletedata'])->name('deletedata');
+});
 
 Route::get('/contact', function () {
     return view('contact', [
