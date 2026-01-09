ea70ae32e0efd319099b96bf27eade43150c21f7
deedatmid <deedatalayyubi@gmail.com>
2026-01-10 02:39:15 +0700

    middleware auth login

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends HttpKernel
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth.login' => \App\Http\Middleware\AuthLogin::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
